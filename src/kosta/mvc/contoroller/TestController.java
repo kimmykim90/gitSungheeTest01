fee7152fc59678c967171414bc96cddbd7210ebe
USER <79305476+kimmykim90@users.noreply.github.com>
2021-03-12 10:39:47 +0900

    첫번째 세팅하기

---
+++ src/kosta/mvc/contoroller/TestController.java
@@ -0,0 +1,8 @@
+package kosta.mvc.contoroller;
+
+public class TestController {
+	public void insert() {
+		System.out.println("insert¿‘¥œ¥Ÿ");
+	}
+
+}
---
+++ src/kosta/mvc/view/MainView.java
@@ -0,0 +1,10 @@
+package kosta.mvc.view;
+
+public class MainView {
+
+	public static void main(String[] args) {
+		System.out.println("어려워요");
+
+	}
+
+}
